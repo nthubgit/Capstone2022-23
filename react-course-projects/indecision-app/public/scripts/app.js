e514c0a308de056ce0a12eaeed1e1b37b7bb06de
nthubgit <neltyler@gmail.com>
2022-11-25 19:46:26 -0500

    various

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -1,49 +1,45 @@
-"use strict";
+'use strict';
 
-//arguments object - no longer bound with arrow functions
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-// const add = function (a, b) { es5
-//     console.log(arguments);
-//     return a + b;
-// }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-// const add = (a, b) => { es6
-//     console.log(arguments);
-//     return a + b;
-// }
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-var add = function add(a, b) {
-  return a + b;
-};
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-console.log(add(2, 3));
+var Header = function (_React$Component) {
+    _inherits(Header, _React$Component);
 
-var user = {
-  name: "Bob",
-  cities: ["New York", "Montreal", "Vermont"],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
+    function Header() {
+        _classCallCheck(this, Header);
 
-    return this.cities.map(function (city) {
-      return _this.name + " has lived in " + city;
-    });
-  }
-};
+        return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+    }
 
-console.log(user.printPlacesLived());
+    _createClass(Header, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'p',
+                null,
+                'This is from Header.'
+            );
+        }
+    }]);
 
-//chalelnge
+    return Header;
+}(React.Component);
 
-var multiplier = {
-  numbers: [2, 4, 6, 8, 10],
-  multiplyBy: 10,
-  multiply: function multiply() {
-    var _this2 = this;
+var jsx = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Title'
+    ),
+    React.createElement(Header, null)
+);
 
-    return this.numbers.map(function (number) {
-      return number * _this2.multiplyBy;
-    });
-  }
-};
-
-console.log(multiplier.multiply());
+ReactDOM.render(jsx, document.getElementById('app'));
--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,44 +1,14 @@
-console.log("App.js is running")
+class Header extends React.Component {
+    render () {
+        return <p>This is from Header.</p>;
+    }
+}
 
-//JSX - JavaScript XML
-const app = {
-    title: "Indecision App",
-    subtitle: <p>"Put your life in the hands of a computer"</p>,
-    options: ["One", "Two"]
-};
-const template = (
+const jsx = (
     <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && app.subtitle}
-        <p>{(app.options.length > 0) ? "Here are your options:" : "No options"}</p>
-        <ol>
-            <li>Item One</li>
-            <li>Item Two</li>
-        </ol>
+    <h1>Title</h1>
+    <Header/>
     </div>
 );
 
-//demo stuff
-const user = {
-    name: 'Tyler',
-    age: 26,    
-    location: 'Canada'
-};
-
-function getLocation(location){
-    if (location){
-       return  <p>Location: {location}</p>; 
-    }
-    return undefined;
-}
-
-const template2 = (
-    <div>
-    <h1>{user.name ? user.name : "Anonymous"}</h1>
-    {(user.age && user.age >=18) && <p>Age: {user.age}</p>}
-    {getLocation(user.location)}
-    </div>
-)
-const appRoot = document.getElementById("app");
-
-ReactDOM.render(template, appRoot);
+ReactDOM.render (jsx, document.getElementById('app'));
