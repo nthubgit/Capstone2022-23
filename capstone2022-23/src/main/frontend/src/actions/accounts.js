50fd083273baef8411677158dfe5cb9fbf366f77
nthubgit <neltyler@gmail.com>
2023-01-04 21:55:37 -0500

    See prev

--- capstone2022-23/src/main/frontend/src/actions/accounts.js
+++ capstone2022-23/src/main/frontend/src/actions/accounts.js
@@ -1,4 +1,5 @@
 import uuid from 'uuid';
+import axios from 'axios';
 
 //component calls action gneerator
 //action generator returns object
@@ -10,10 +11,28 @@ import uuid from 'uuid';
 //component dispatches function?
 //function runs (can dispatch other actions)
 
+export const getUsers = () => async dispatch => {
+    
+    try{
+        const res = await axios.get(`http://jsonplaceholder.typicode.com/users`)
+        dispatch( {
+            type: GET_ALL_ACCOUNTS,
+            payload: res.data
+        })
+    }
+    catch(e){
+        dispatch( {
+            type: USERS_ERROR,
+            payload: console.log(e),
+        })
+    }
+
+}
+
 //add_account
-export const addAccount = (user) => ({
+export const addAccount = (account) => ({
     type: 'ADD_ACCOUNT',
-    user
+    account
 })
 //edit_account
 export const editAccount = (id, updates) => ({
